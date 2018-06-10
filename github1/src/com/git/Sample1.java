fd38afa2357d7a890e7e0d5c35824b560f314170
padmavathigonugunta58gmail.com <padmavathigonugunta58gmail.com@192.168.43.93>
2018-06-10 17:58:26 +0530

    track2

--- github1/src/com/git/Sample1.java
+++ github1/src/com/git/Sample1.java
@@ -3,8 +3,10 @@ package com.git;
 public class Sample1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      System.out.println("welcome to dell");
+		// TODO Auto-generated method stubpaddu
+      System.out.println("welcome to apple");
+     
+      
 	}
 
 }
