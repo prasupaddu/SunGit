c42cfb6caea629dd40af33aab72368360ac3da12
pras <Prasanna@DESKTOP-77UOMJ5>
2018-06-10 17:14:00 +0530

    second attempt

--- Sungithub/src/com/git/cpp/Samplegit.java
+++ Sungithub/src/com/git/cpp/Samplegit.java
@@ -5,6 +5,8 @@ public class Samplegit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         System.out.println("hello");
+        int a=10;
+        System.out.println(a);
 	}
 
 }
