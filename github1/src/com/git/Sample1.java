da2ef31e6b1b8d0972c9260cf04afefee57d3b3c
pras <Prasanna@DESKTOP-77UOMJ5>
2018-06-10 18:23:46 +0530

    track4

--- github1/src/com/git/Sample1.java
+++ github1/src/com/git/Sample1.java
@@ -5,7 +5,7 @@ public class Sample1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubpaddu
       System.out.println("welcome to apple");
-     
+      System.out.println("hiihello");
       
 	}
 
